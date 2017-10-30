af496b5e75aa3f296b0cf2ab07b17ccc86f0e502
Cuahchic <colinparry33@gmail.com>
2017-10-30 15:57:45 +0000

    Written additional function to parse a list of results and return the data found to a custom class

--- car-pricing.py
+++ car-pricing.py
@@ -12,20 +12,10 @@ import csv
 import random
 import datetime
 import requests
+import re
+from re import sub
+from decimal import Decimal
 
-# Logging of http fails
-import logging
-try: # for Python 3
-    from http.client import HTTPConnection
-except ImportError:
-    from requests.packages.urllib3.connectionpool import HTTPConnection
-HTTPConnection.debuglevel = 1
-
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
 
 # Set up all the search parameters, note that many of these are case sensitive
 milesFrom = '100'    # Set to 1500 to see national, can be set to any number (e.g. 157)
@@ -58,6 +48,27 @@ with open('UserAgents.csv', 'r') as f:
     user_agents = list(reader)      # Returns this as a list of lists (if the CSV had multiple columns these would be in sublist)
 
 
+# Create a class that we can use to track everything we want to save from each advert
+class advert:
+    def __init__(self):
+        self.adTitle = ''
+        self.adID = ''
+        self.attentionGrab = ''
+        self.year = 1970
+        self.plate = 70
+        self.bodyType = ''
+        self.mileage = 0
+        self.transmission = ''
+        self.engineSize = 0
+        self.bhp = 0
+        self.fuelType = ''
+        self.price = Decimal(0)
+        self.sellerType = ''
+        self.distanceFromYou = 0
+        self.Location = ''
+
+
+
 # Takes the search criteria selected by the user and builds the Autotrader URL to scrape
 def urlBuilder():
     outputURL = 'https://www.autotrader.co.uk/car-search?sort=price-asc'   # Always sort ascending order in price
@@ -88,9 +99,80 @@ def urlBuilder():
 # Randomly choose one of the previously defined user agents
 def pickAgent():
     return random.choice(user_agents)[0]
+
+
+# Find the maximum number of pages from a search result page
+def findMaxPages(soup):
+    pagination = soup.find('li', attrs = {'class': 'paginationMini__count'})
     
+    maxPages = 0
+    
+    if len(pagination) > 0:
+        rPag = re.compile(r'\d+$')
+        rPagResult = rPag.search(pagination.text.strip())
+        
+        if rPagResult != None:
+            maxPages = int(rPagResult[0])
+    
+    return maxPages
 
 
+# Take a single results page and scrape it for the relevant information we want to get back, returns a list of objects
+def parsePage(soup):
+    resultsListToReturn = []
+    
+    pageResults = soup.findAll('li', attrs = {'class': 'search-page__result'})
+    
+    for j in range(0, len(pageResults)):
+        ad = advert()
+        contentCol = pageResults[j].find('section', attrs = {'class': 'content-column'})
+        priceCol = pageResults[j].find('section', attrs = {'class': 'price-column'})
+        
+        ad.adID = pageResults[j].get('id')
+        ad.adTitle = contentCol.find('h2', attrs = {'class': 'listing-title title-wrap'}).text.strip()
+        ad.attentionGrab = contentCol.find('p', attrs = {'class': 'listing-attention-grabber'}).text.strip()
+        
+        # Get unnamed list of attributes
+        liTags = contentCol.find('ul', attrs = {'class': 'listing-key-specs'}).findAll('li')
+        
+        # Regexii for sanity checking the list
+        rYear = re.compile(r'(\d+) \((\d+) reg\)')
+        rBodyType = re.compile(r'(convertible|coupe|estate|hatchback|mpv|other|suv|saloon|unlisted)')
+        rMileage = re.compile(r'\d+,\d+ miles')
+        rTransmission = re.compile(r'(manual|automatic)')
+        rEngineSizeLitres = re.compile(r'\d+\.\d+l')
+        rHorsepowerBHP = re.compile(r'\d+ bhp')
+        rFuelType = re.compile(r'(petrol|diesel)')
+        
+        for li in liTags:
+            if rYear.search(li.text.lower()) != None:
+                rYearMatches = rYear.match(li.text.lower())
+                
+                ad.year = rYearMatches.groups()[0]
+                ad.plate = rYearMatches.groups()[1]
+            elif rBodyType.search(li.text.lower()) != None:
+                ad.bodyType = li.text
+            elif rMileage.search(li.text.lower()) != None:
+                ad.mileage = int(li.text.replace(' miles', '').replace(',', ''))
+            elif rTransmission.search(li.text.lower()) != None:
+                ad.transmission = li.text
+            elif rEngineSizeLitres.search(li.text.lower()) != None:
+                ad.engineSize = float(li.text.replace('L', ''))
+            elif rHorsepowerBHP.search(li.text.lower()) != None:
+                ad.bhp = int(li.text.replace(' bhp', ''))
+            elif rFuelType.search(li.text.lower()) != None:
+                ad.fuelType = li.text
+        
+        currencyString = priceCol.find('div', attrs = {'class': 'vehicle-price'}).text
+        ad.price = Decimal(sub(r'[^\d.]', '', currencyString))
+        
+        
+        resultsListToReturn.append(ad)
+        
+    return resultsListToReturn
+
+
+# The main code block we want to run
 def main():
     # Firstly get a user agent
     user_agent = pickAgent()
@@ -103,9 +185,61 @@ def main():
 
     # Create soup
     soup = BeautifulSoup(response.text, "html.parser")
+    
+    # Find pagination results so we know how many pages to iterate through
+    maxPages = findMaxPages(soup)
+    
+    # Initialise empty results list
+    masterResultsList = []
+    
+    # Parse first page
+    resultsList = parsePage(soup)
+    
+    # Concatenate lists together
+    masterResultsList = masterResultsList + resultsList
+    
+    if maxPages > 1:
+        # Loop through all the pages and add the results to a list
+        for pgNum in range(2, maxPages + 1):     # Since Python only goes to less than the latter number
+            # Append the page number onto the URL to get subsequent pages
+            currentSearchURL = searchURL + '&page=' + pgNum
+        
+            # Now run load webpage
+            response = requests.get(searchURL, headers = {'User-Agent': user_agent})
+        
+            # Create soup
+            soup = BeautifulSoup(response.text, "html.parser")
+
+
+
 
 main()
     
     
     
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
