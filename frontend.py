fb55cd7f699a4c4eb63b3ac087dd52362c917302
Cuahchic <colinparry33@gmail.com>
2018-01-10 15:59:25 +0000

    Changes to visualisation to get scatter plot working. Almost got the sliders working but requires a bit more work because the x and y axes are not between 0 and 1. Hopefully tomorrow get onto the donut chart and Google maps integration.

--- frontend.py
+++ frontend.py
@@ -14,6 +14,9 @@ Created on Mon Jan 16 10:32:43 2017
 
 from flask import Flask, jsonify, render_template
 from cassandra.cluster import Cluster
+from urllib import parse
+import pandas as pd
+import datetime
 
 app = Flask(__name__)
 
@@ -32,7 +35,7 @@ def listsearches():
     cql = 'SELECT DISTINCT searchname, advertid FROM car_pricing.searchdata;'
 
     prepStatement = session.prepare(cql)
-    queryResults = session.execute(prepStatement)
+    queryResults = session.execute(prepStatement) 
     
     searches = set()
     for qr in queryResults:
@@ -49,24 +52,67 @@ def listsearches():
 # This API gets all the data required in the front end
 @app.route('/api/getdata/<string:searchname>', methods=['GET'])
 def getdata(searchname):
+    searchname = parse.unquote(searchname)      # Decode URL, e.g. turn %20 into space etc
+    
     cluster = Cluster()                         # Connect to local host on default port 9042
     session = cluster.connect('car_pricing')    # Connect to car_pricing keyspace
     
+    # Get adIDs into list related to our search name
     cql = 'SELECT DISTINCT searchname, advertid FROM car_pricing.searchdata;'
 
     prepStatement = session.prepare(cql)
     queryResults = session.execute(prepStatement)
     
-    searches = set()
+    adIDs = []
     for qr in queryResults:
-        searches.add(qr[0])
+        if qr[0] == searchname:     # If this is a result from our desired search then add it to the list
+            adIDs.append(qr[1])
+    
+    # List columns we want to use as features (or to create features from) and build up cql query
+    colListOther = ['advertid', 'plate', 'bodytype', 'transmission', 'fueltype', 'sellertype', 'make', 'model', 'dealername', 'features', 'adtitle']
+    colListPlottable = ['year', 'mileage', 'enginesize', 'bhp', 'price', 'averagempg']
+    colListPlottableFriendly = ['Registration Year', 'Mileage (miles)', 'Engine Size (L)', 'Engine Power (BHP)', 'Price (£)', 'Average Fuel Consumption (mpg)']
+    cql = 'SELECT ' + ','.join(colListPlottable + colListOther) + ' FROM car_pricing.searchdata WHERE searchname = ? AND advertid = ? LIMIT 1;'
+    
+    prepStatement = session.prepare(cql)
+    
+    # Create data frame to store results
+    df_D3data = pd.DataFrame(columns = (colListPlottable + colListOther))
+    
+    for adID in adIDs:     # Query to get the latest information (latest data gathering time) for each advert
+        queryResults = session.execute(prepStatement, [searchname, adID])
+        
+        df_D3data = df_D3data.append(pd.DataFrame(data = [list(queryResults[0])], columns = (colListPlottable + colListOther)))   # Note that list is embedded in another list
+        
+    # Add advert age to the data frame
+    df_D3data['advertage_days'] = df_D3data['advertid'].apply(compare_dates)
+    colListPlottable += ['advertage_days']
+    colListPlottableFriendly += ['Advert Age (days)']
     
     session.shutdown()
     cluster.shutdown()
     
-    response = jsonify({"searches": list(searches)})
+    # Remove any points which are not valid, i.e. NaN, None, etc
+    df_D3data = df_D3data[df_D3data.notnull().all(axis = 1)]
+    
+    df_D3data = df_D3data.reset_index()     # Required to generate index for DF so that it can be turned into JSON
+    
+    # Prepare columns for output by sorting in alphabetical order and putting into dictionary for output
+    colListPlottableFriendly, colListPlottable = (list(x) for x in zip(*sorted(zip(colListPlottableFriendly, colListPlottable), key = lambda pair: pair[0]))) # Taken from https://stackoverflow.com/questions/13668393/python-sorting-two-lists    
+    colOutputList = [{'name': n, 'friendly_name': fn} for n, fn in zip(colListPlottable, colListPlottableFriendly)]
+    
+    response = jsonify({'data': df_D3data.to_dict(orient = 'records'),
+                        'plottable_columns': colOutputList})
     
     return response    
+
+
+# Calculate the age of the advert
+def compare_dates(advertid):
+    date = datetime.datetime.strptime(advertid[0:8], '%Y%m%d')    
+    today = datetime.datetime.now()
+    diff = today - date
+    return diff.days
 
 
 # Main code
--- static/carprices.js
+++ static/carprices.js
@@ -1,36 +1,41 @@
+// Global variables
 var margin = { top: 20, right: 20, bottom: 50, left: 50 },
-    statsMargin = { top: 10, right: 10, bottom: 10, left: 10, donutSize: 40 },
-    leg = {rectSize: 10, rectSpacing: 4, rectTextSize: 11},
     basewidth = 565,
     baseheight = 565,
     width = basewidth - margin.left - margin.right,
     height = baseheight - margin.top - margin.bottom,
+	
+	statsWidth = 278,
+    statsHeight = 278,
+    statsMargin = { top: 10, right: 10, bottom: 10, left: 10, donutSize: 40 },
+    statsRadius = d3.min([statsWidth - statsMargin.left - statsMargin.right, statsHeight - statsMargin.top - statsMargin.bottom]) / 2,
+	
+    svg,
+	allDots,
+	
+    xaxis_class,
+    yaxis_class,
+    xaxislabel,
+    yaxislabel,
+	
+	xMin = {},
+    xMax = {},
+    yMin = {},
+    yMax = {},
+	xRange = {},
+	yRange = {},
+	
     xLabel,
     yLabel,
     xLabelTxt,
     yLabelTxt,
-    svg,
-    myDuration = 500,
-    alldata,
-    statdata = {},
-    xaxis,
-    yaxis,
-    xaxislabel,
-    yaxislabel,
-    freezeMouseover = 0,
-    allDots,
-    xMin = 0,
-    xMax = 1,
-    yMin = 0,
-    yMax = 1,
-    statsWidth = 278,
-    statsHeight = 278,
-    statsRadius = d3.min([statsWidth - statsMargin.left - statsMargin.right, statsHeight - statsMargin.top - statsMargin.bottom]) / 2,
-    statCharts = ["ProductLine", "Rating_kW", "EngineDetails", "AICCode", "Region"],
-    colors = {},
-    firstStats = 1,
-    userView = "loadProfiles",
-    statsMeasure = "count";
+	
+	plottableColumns = [],
+	data = {},
+	
+	freezeMouseover = 0,
+	
+	clustersDiv = d3.select("#clustersChart");
 
 // setup x 
 var xValue = function (d) { return d[xLabel]; }, // data -> value
@@ -53,12 +58,6 @@ var arc = d3.arc()
 var pie = d3.pie()
     .value(function (d) { return d[statsMeasure]; });
 
-// Get DOM elements at global variables so we don't need to keep finding them every time we want to use them
-var clustersDiv = d3.select("#clustersChart");
-var kf = d3.select("#keyfacts");
-var chart = d3.select("#chartlink");
-var caclStatsButton = d3.select("#calcButton");
-
 // Read in data
 listSearches();
 
@@ -71,8 +70,6 @@ $(function () {
         values: [0, 1],
         step: 0.01,
         slide: function (event, ui) {
-            caclStatsButton.attr("disabled", null);
-
             xMin = ui.values[0];
             xMax = ui.values[1];
 
@@ -90,8 +87,6 @@ $(function () {
         values: [0, 1],
         step: 0.01,
         slide: function (event, ui) {
-            caclStatsButton.attr("disabled", null);
-
             yMin = ui.values[0];
             yMax = ui.values[1];
 
@@ -119,7 +114,7 @@ function listSearches() {
 		var signalSelect = axisSelector
 			.append("select")
 			.attr("id", "chooseSearchName")
-			.on("change", "getData();");
+			.on("change", getData);
 
 		var signalOptions = signalSelect
 			.selectAll("option")
@@ -137,7 +132,7 @@ function listSearches() {
 			.append("select")
 			.attr("name", "xaxis")
 			.attr("id", "xaxisSel")
-			.on("change", "updateScattergram();");
+			.on("change", updateScattergram);
 			
 		axisSelector.append("br");
 		
@@ -149,7 +144,7 @@ function listSearches() {
 			.append("select")
 			.attr("name", "yaxis")
 			.attr("id", "yaxisSel")
-			.on("change", "updateScattergram();");
+			.on("change", updateScattergram);
 		
 		// Force the on change event to fire and populate the x and y axis boxes
 		getData();
@@ -157,37 +152,257 @@ function listSearches() {
 }
 
 
+// Query the database to get the relevant data related to a search
 function getData() {
 	var chooseSearchName = $("#chooseSearchName");
 	var selectedSearchName = chooseSearchName[0].options[chooseSearchName[0].selectedIndex].text;
 	
 	$.getJSON("/api/getdata/" + encodeURI(selectedSearchName), function (json) {
-		var a = 1;
+		data = json["data"];
+		plottableColumns = json["plottable_columns"];
+		
+		populateXandYSelectors();
+		createScattergram();
 	});
 }
 
 
+// Function to put the plottable columns as options in the X and Y axis selectors
+function populateXandYSelectors() {
+	var xaxisSel = d3.select("#xaxisSel");
+	
+	xaxisSel
+		.selectAll("option")
+		.data(plottableColumns)
+		.enter()
+		.append("option")
+		.property("value", function(d) { return d.name; })
+		.text(function (d) { return d.friendly_name; });
+	
+	var yaxisSel = d3.select("#yaxisSel");
+	
+	yaxisSel
+		.selectAll("option")
+		.data(plottableColumns)
+		.enter()
+		.append("option")
+		.property("value", function(d) { return d.name; })
+		.text(function (d) { return d.friendly_name; });
+		
+	$('select[id=xaxisSel] option:eq(0)').attr('selected', 'selected');  // Select first option for x axis
+	$('select[id=yaxisSel] option:eq(1)').attr('selected', 'selected');  // Select second option for y axis
+}
 
 
+// Function to create the scattergram
+function createScattergram() {
+    // Get selected elements
+    getSelectedAxisOptions();
+
+    // Add svg elements to div
+    svg = clustersDiv.append("svg")
+        .attr("width", basewidth)
+        .attr("height", baseheight)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Find min and max values to set the domain of each axis
+    resizeDomains();
+
+    // x-axis
+    xaxis_class = svg.append("g")
+        .attr("class", "axis")
+        .attr("id", "xaxis")
+        .attr("transform", "translate(0," + height + ")");
+        
+    // y-axis
+    yaxis_class = svg.append("g")
+        .attr("class", "axis")
+        .attr("id", "yaxis");
+    
+    // Add axis labels
+    createAxisLabels();
+
+    // Do the axis creation after adding text so the text appears on top --- NEEDS REVISITED
+    /*xAxis.tickFormat(d3.format(".2f"));
+    yAxis.tickFormat(d3.format(".2f"));*/
+    xaxis_class.call(xAxis);
+    yaxis_class.call(yAxis);
+
+    // Add a clip path so that when we zoom any circles that go out of the chart area are not drawn
+    var clip = svg.append("svg:clipPath")
+        .attr("id", "clip")
+        .append("svg:rect")
+        .attr("id", "clip-rect")
+        .attr("x", "0px")
+        .attr("y", "0px")
+        .attr("width", width)    // This accounts for 0.01 margin left and right, top and bottom
+        .attr("height", height);
+
+    // draw dots
+    svg.selectAll(".dot")
+        .data(data)
+        .enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", 1.5)
+        .attr("cx", xMap)
+        .attr("cy", yMap)
+        .attr("clip-path", "url(#clip)")
+        .on("mouseover", mouseover)
+        .on("mouseleave", mouseleave)
+        .on("click", click);
+
+    // Get all dots for mouseover
+    selectDots();
+}
 
 
+// Function to get the selected values of x and y axis from the drop down menus
+function getSelectedAxisOptions() {
+    xLabel = $("#xaxisSel")[0].value;
+    yLabel = $("#yaxisSel")[0].value;
+    xLabelTxt = $("#xaxisSel")[0].options[$("#xaxisSel")[0].selectedIndex].text;
+    yLabelTxt = $("#yaxisSel")[0].options[$("#yaxisSel")[0].selectedIndex].text;
+}
 
 
+// Function to scale domains based on min and max values of chosen columns
+function resizeDomains() {
+	xMin = data.reduce(function(prev, curr) { return prev[xLabel] < curr[xLabel] ? prev : curr; })[xLabel];
+	xMax = data.reduce(function(prev, curr) { return prev[xLabel] > curr[xLabel] ? prev : curr; })[xLabel];
+	yMin = data.reduce(function(prev, curr) { return prev[yLabel] < curr[yLabel] ? prev : curr; })[yLabel];
+	yMax = data.reduce(function(prev, curr) { return prev[yLabel] > curr[yLabel] ? prev : curr; })[yLabel];
+	
+	xRange = xMax - xMin;
+	yRange = yMax - yMin;
+	
+	xScale.domain([xMin - (0.01 * xRange), xMax + (0.01 * xRange)]);
+    yScale.domain([yMin - (0.01 * yRange), yMax + (0.01 * yRange)]);
+}
 
 
+// Function which creates the x and y axis labels
+function createAxisLabels() {
+    xaxislabel = xaxis_class.append("text")
+                    .attr("class", "axislabel")
+                    .attr("x", width / 2)
+                    .attr("y", margin.bottom * 0.7)
+                    .text(xLabelTxt);
+
+    yaxislabel = yaxis_class.append("text")
+                    .attr("class", "axislabel")
+                    .attr("transform", "rotate(-90)")
+                    .attr("x", -height / 2)
+                    .attr("y", -margin.left * 0.7)
+                    .text(yLabelTxt);
+}
 
 
+// Function to select all dots so we don't need to do this every mouseover
+function selectDots() {
+    allDots = d3.selectAll(".dot");
+}
 
 
+// Function to update chart when new x or y axis variable selected
+function updateScattergram() {
+    // Get selected elements
+    getSelectedAxisOptions();
+	
+	// Resize the domain for the new data
+	resizeDomains();
+	
+	// Relabel the numbers on each axis
+	xaxis_class.call(xAxis);
+    yaxis_class.call(yAxis);
+	
+    // Get existing elements
+    var dots = svg.selectAll(".dot").data(data);
+    dots
+        .attr("cx", xMap)
+        .attr("cy", yMap);
+
+    // New elements
+    var dotEnter = dots.enter();
+    dotEnter.append("circle")
+        .attr("class", "dot")
+        .attr("r", 1.5)
+        .attr("cx", xMap)
+        .attr("cy", yMap)
+        .on("mouseover", mouseover)
+        .on("mouseleave", mouseleave)
+        .on("click", click);
+
+    dots.exit().remove();
+
+    // Add axis labels
+    updateAxisLabels();
+
+    // Get all dots for mouseover
+    selectDots();
+}
 
 
+// Function which amends the x and y axis labels
+function updateAxisLabels() {
+    xaxislabel.text(xLabelTxt);
+    yaxislabel.text(yLabelTxt);
+}
 
 
+// Function to add text to right hand side information on rollover of circle
+function mouseover(d) {
+    if (freezeMouseover == 0) {
+        // Fade all circles
+        allDots.attr("opacity", 0.1);
+
+        // Make the mouseover circle full opacity
+        d3.select(this).attr("opacity", 1);
+    }
+};
+
+// Function to reset page when cursor moved away from circle
+function mouseleave(d) {
+    if (freezeMouseover == 0) {
+        // Bring all circles back to full opacity
+        allDots.attr("opacity", 1);
+    };
+};
+
+// Function to freeze mouseover text
+function click(d) {
+    // Change value of freezeMouseover variable
+    freezeMouseover = (freezeMouseover + 1) % 2;
+};
 
 
+// Function to zoom axis
+function zoom(begin, end, whichAxis) {
+    // Calculate new tick values for axis
+	var adj_begin = xMin + begin * xRange;		// Begin is in the range [0,1] and represents the fractional slider range, adj_begin converts this into the units of the axis
+	var adj_end = xMax - (1 - end) * xRange;
+	
+    var rng = adj_end - adj_begin;
+    var step = rng / 10;
+    var ticks = [];
+    for (var i = 0; i <= 10; i++) {
+        ticks.push(adj_begin + step * i);
+    }
 
-
-
+    // Change scale domain and assign tick values calculated above
+    if (whichAxis == "x") {
+        xScale.domain([(-0.01 / 1.02 * rng) + adj_begin, adj_end + (0.01 / 1.02 * rng)]);
+        xAxis.tickValues(ticks);
+        xaxis_class.call(xAxis);
+        allDots.attr("cx", xMap);
+    }
+    else if (whichAxis == "y") {
+        yScale.domain([(-0.01 / 1.02 * rng) + adj_begin, adj_end + (0.01 / 1.02 * rng)]);
+        yAxis.tickValues(ticks);
+        yaxis_class.call(yAxis);
+        allDots.attr("cy", yMap);
+    }
+}
 
 
 
