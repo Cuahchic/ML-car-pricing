2c70ee4b64ce5284811c9a567e4237b610533af8
Cuahchic <colinparry33@gmail.com>
2018-01-18 15:59:47 +0000

    After being IP blocked by Autotrader for scraping too many results, I felt it was better to progress the ML parts rather than keep trying to obtain new data (after all we want to see some adverts get price changes and these take time). Did some comparisons of different ML models to predict prices, such as random forest, gradient boost and various implementations of KNN. More work required.

--- ml-testing/MLtests.py
+++ ml-testing/MLtests.py
@@ -12,12 +12,25 @@ import pandas as pd
 import datetime
 import seaborn as sns
 
-import sklearn
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import MinMaxScaler
+
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.neighbors import KNeighborsRegressor
+
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
+
+from ggplot import ggplot, aes, geom_point
 
 cluster = Cluster()                         # Connect to local host on default port 9042
 session = cluster.connect('car_pricing')    # Connect to car_pricing keyspace
@@ -35,7 +48,7 @@ for qr in queryResults:
 
 # Get unique searches so the user can filter this on the visualisation
 searchNames = list(df_adIDs['SearchName'].unique())
-searchName = searchNames[0]
+searchName = searchNames[1]
 
 # List columns we want to use as features (or to create features from) and build up cql query
 colList = ['advertid', 'foundtime', 'year', 'plate', 'bodytype', 'mileage', 'transmission', 'enginesize', 'bhp', 'fueltype', 'price', 'sellertype', 'make', 'model', 'dealername', 'features', 'averagempg']
@@ -46,7 +59,7 @@ prepStatement = session.prepare(cql)
 # Create data frame to store results
 df_searchData = pd.DataFrame(columns = colList)
 
-for adID in df_adIDs[df_adIDs['SearchName'] == 'Local automatics']['AdvertID']:     # Query to get the latest information (latest data gathering time) for each advert
+for adID in df_adIDs[df_adIDs['SearchName'] == searchName]['AdvertID']:     # Query to get the latest information (latest data gathering time) for each advert
     queryResults = session.execute(prepStatement, [searchName, adID])
     
     df_searchData = df_searchData.append(pd.DataFrame(data = [list(queryResults[0])], columns = colList))   # Note that list is embedded in another list
@@ -64,9 +77,9 @@ df_searchData['advertage_days'] = df_searchData['advertid'].apply(compare_dates)
 
 
 # Let's do some initial data exploration
-sns.regplot(x = 'mileage', y = 'price', data = df_searchData, fit_reg = False)
-sns.regplot(x = 'advertage_days', y = 'price', data = df_searchData, fit_reg = False)
-plt = sns.swarmplot(x = 'make', y = 'price', data = df_searchData)
+ggplot(df_searchData, aes(x='mileage', y='price')) + geom_point()
+ggplot(df_searchData, aes(x='advertage_days', y='price')) + geom_point()
+plt = sns.swarmplot(x = 'make', y = 'price', data = df_searchData)      # No ggplot equivalent for this chart type that I can find
 plt.set_xticklabels(plt.get_xticklabels(), rotation = 90)
 
 
@@ -82,18 +95,96 @@ X, Y = shuffle(X, Y)                    # Randomise the order of the rows
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.05, random_state = 32)
 
-rf = RandomForestRegressor(n_estimators = 500, max_features = 'sqrt')
-rf = rf.fit(X_train, Y_train)
-Y_pred_rf = rf.predict(X_test)
-scores_rf = cross_val_score(rf, X_test, Y_test, cv = 3, scoring = 'r2')
+# Predictions using random forest regression
+rf_params = {'n_estimators': [10, 50, 100, 500, 1000],
+             'criterion': ['mse'],
+             'max_features': [None, 'sqrt', 'log2'],
+             'min_samples_split': [2, 5, 10, 20]}
+rf = RandomForestRegressor()
+rf_gscv = GridSearchCV(rf, rf_params)
+rf_gscv = rf_gscv.fit(X_train, Y_train)
+Y_pred_rf = rf_gscv.predict(X_test)
+scores_rf = cross_val_score(rf_gscv, X_test, Y_test, cv = 3, scoring = 'r2')
+r2_rf = r2_score(Y_test, Y_pred_rf)
+ggplot(pd.DataFrame({'actual': Y_test, 'predicted': Y_pred_rf}), aes(x='actual', y='predicted')) + geom_point()
 
-gbc = GradientBoostingRegressor(loss = 'huber', n_estimators = 500, max_features = 'sqrt')
-gbc = gbc.fit(X_train, Y_train)
-Y_pred_gbc = gbc.predict(X_test)
+# Predictions using gradient boosted regression
+gbc_params = {'loss': ['huber'],
+              'n_estimators': [500, 1000, 5000],
+              'max_features': [None, 'sqrt', 'log2'],
+              'min_samples_split': [2, 5, 10, 20],
+              'alpha': [0.1, 0.5, 0.9]}
+gbc = GradientBoostingRegressor()
+gbc_gscv = GridSearchCV(gbc, gbc_params)
+gbc_gscv = gbc_gscv.fit(X_train, Y_train)
+Y_pred_gbc = gbc_gscv.predict(X_test)
 scores_gbc = cross_val_score(gbc, X_test, Y_test, cv = 3, scoring = 'r2')
+r2_gbc = r2_score(Y_test, Y_pred_gbc)
+ggplot(pd.DataFrame({'actual': Y_test, 'predicted': Y_pred_gbc}), aes(x='actual', y='predicted')) + geom_point()
 
-sns.regplot(x = Y_test, y = Y_pred_rf, fit_reg = False)
-sns.regplot(x = Y_test, y = Y_pred_gbc, fit_reg = False)
+# Predictions using k-Nearest Neighbours (no dimesnionality reduction or scaling)
+knn_params = {'n_neighbors': [2, 5, 10],
+              'weights': ['uniform', 'distance'],
+              'algorithm': ['ball_tree', 'kd_tree', 'brute']}
+knnr = KNeighborsRegressor()
+knnr_gscv = GridSearchCV(knnr, knn_params)
+knnr_nored_noscale_gscv = knnr_gscv.fit(X_train, Y_train)
+Y_pred_knnr_nored_noscale = knnr_nored_noscale_gscv.predict(X_test)
+scores_knnr_nored_noscale = cross_val_score(knnr, X_test, Y_test, cv = 3, scoring = 'r2')
+r2_knnr_nored_noscale = r2_score(Y_test, Y_pred_knnr_nored_noscale)
+ggplot(pd.DataFrame({'actual': Y_test, 'predicted': Y_pred_knnr_nored_noscale}), aes(x='actual', y='predicted')) + geom_point()
+
+# Predictions using k-Nearest Neighbours (no dimesnionality reduction but standard scaling)
+x_scale_std = StandardScaler().fit(X)   # No need to scale Y values
+
+knnr_nored_stdscale_gscv = knnr_gscv.fit(x_scale_std.transform(X_train), Y_train)
+Y_pred_knnr_nored_stdscale = knnr_nored_stdscale_gscv.predict(x_scale_std.transform(X_test))
+scores_knnr_nored_stdscale = cross_val_score(knnr, x_scale_std.transform(X_test), Y_test, cv = 3, scoring = 'r2')
+r2_knnr_nored_stdscale = r2_score(Y_test, Y_pred_knnr_nored_stdscale)
+ggplot(pd.DataFrame({'actual': Y_test, 'predicted': Y_pred_knnr_nored_stdscale}), aes(x='actual', y='predicted')) + geom_point()
+
+# Predictions using k-Nearest Neighbours (no dimesnionality reduction but normalised scaling)
+x_scale_norm = Normalizer().fit(X)   # No need to scale Y values
+
+knnr_nored_normscale_gscv = knnr_gscv.fit(x_scale_norm.transform(X_train), Y_train)
+Y_pred_knnr_nored_normscale = knnr_nored_normscale_gscv.predict(x_scale_norm.transform(X_test))
+scores_knnr_nored_normscale = cross_val_score(knnr, x_scale_norm.transform(X_test), Y_test, cv = 3, scoring = 'r2')
+r2_knnr_nored_normscale = r2_score(Y_test, Y_pred_knnr_nored_normscale)
+ggplot(pd.DataFrame({'actual': Y_test, 'predicted': Y_pred_knnr_nored_normscale}), aes(x='actual', y='predicted')) + geom_point()
+
+# Predictions using k-Nearest Neighbours (no dimesnionality reduction but MinMax scaling)
+x_scale_minmax = MinMaxScaler().fit(X)   # No need to scale Y values
+
+knnr_nored_minmaxscale_gscv = knnr_gscv.fit(x_scale_minmax.transform(X_train), Y_train)
+Y_pred_knnr_nored_minmaxscale = knnr_nored_minmaxscale_gscv.predict(x_scale_minmax.transform(X_test))
+scores_knnr_nored_minmaxscale = cross_val_score(knnr, x_scale_minmax.transform(X_test), Y_test, cv = 3, scoring = 'r2')
+r2_knnr_nored_minmaxscale = r2_score(Y_test, Y_pred_knnr_nored_minmaxscale)
+ggplot(pd.DataFrame({'actual': Y_test, 'predicted': Y_pred_knnr_nored_minmaxscale}), aes(x='actual', y='predicted')) + geom_point()
+
+# Predictions using k-Nearest Neighbours (PCA dimesnionality reduction and MinMax scaling)
+pca = PCA()
+
+knnr_nored_minmaxscale_gscv = knnr_gscv.fit(x_scale_minmax.transform(X_train), Y_train)
+Y_pred_knnr_nored_minmaxscale = knnr_nored_minmaxscale_gscv.predict(x_scale_minmax.transform(X_test))
+scores_knnr_nored_minmaxscale = cross_val_score(knnr, x_scale_minmax.transform(X_test), Y_test, cv = 3, scoring = 'r2')
+r2_knnr_nored_minmaxscale = r2_score(Y_test, Y_pred_knnr_nored_minmaxscale)
+sns.regplot(x = Y_test, y = Y_pred_knnr_nored_minmaxscale, fit_reg = False)
+ggplot(pd.DataFrame({'actual': Y_test, 'predicted': Y_pred_knnr_nored_minmaxscale}), aes(x='actual', y='predicted')) + geom_point()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
